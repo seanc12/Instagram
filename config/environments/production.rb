25ade20953cae436e2ed00e704eef02e73b5e43a
Sean <sean.cox.or@gmail.com>
2016-05-22 21:05:10 +0000

    add devise authorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: ':host should be set to the actual host of your application.', port: 3000 }
+  
+  
 end
